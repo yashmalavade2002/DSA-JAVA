87fd81b86fd91603d1f64f0109585cb5b5e4122b
Yash Malvade <yashmalvade91@gmail.com>
2025-04-19 17:27:38 +0530

    Create Count Total No of Characters in A String.java

---
+++ String/Count Total No of Characters in A String.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+      // Count Total No of Characters in A String
+      String str ="Hello Yash";
+      int count=0;
+      for(int i=0;i<str.length();i++){
+        if(str.charAt(i)!=' '){
+          count++;
+        }
+      }
+      System.out.println("Number of characters in a string is "+count);
+
+    }
+}
