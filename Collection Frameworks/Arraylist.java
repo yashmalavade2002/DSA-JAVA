50e20fc325dc2131d014f77a9fd248d410846ff0
Yash Malvade <yashmalvade91@gmail.com>
2025-04-20 12:28:31 +0530

    Update

--- Collection Frameworks/Arraylist.java
+++ Collection Frameworks/Arraylist.java
@@ -45,3 +45,55 @@ public class Main {
      System.out.println(list);
     }
 }
+
+
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+     // Arraylist
+      ArrayList<Integer> l1 = new ArrayList<>();
+       // add new elements
+       l1.add(5);
+       l1.add(6);
+       l1.add(7);
+       l1.add(8);
+       // get an element at index i
+       System.out.println(l1.get(1)); // 6
+
+       // print with loop
+      //  for(int i=0;i<l1.size();i++){
+      //   System.out.println(l1.get(i));
+      //  }
+
+       // printing arraylist directly
+       System.out.println(l1);
+
+       // adding element at index i
+       l1.add(1,100);
+       System.out.println(l1);
+
+       // modifying element at index i
+       l1.set(1, 10);
+       System.out.println(l1);
+
+
+       // removing element at index i
+       l1.remove(1);
+       System.out.println(l1);
+
+       // removing element e
+       l1.remove(Integer.valueOf(8));
+       System.out.println(l1);
+
+
+       // checking if an element exist
+       boolean ans=l1.contains(Integer.valueOf(7));
+       System.out.println( ans);
+
+
+       
+
+
+    }
+}
