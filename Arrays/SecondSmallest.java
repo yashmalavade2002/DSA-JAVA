a6b8ab41c5a3cb755cc20a9ef8698c191e14af1c
Yash Malvade <yashmalvade91@gmail.com>
2025-05-01 18:53:53 +0530

    commit

---
+++ Arrays/SecondSmallest.java
@@ -0,0 +1,24 @@
+public class SecondSmallest {
+
+    public static int SecondSmallest(int[] array) {
+        int Smallest = Integer.MAX_VALUE;
+        int SecondSmallest = Integer.MAX_VALUE;
+
+        for (int n : array) {
+            if (n < Smallest) {
+                SecondSmallest = Smallest;
+                Smallest = n;
+            } else if (n < SecondSmallest && n != Smallest) {
+                SecondSmallest = n;
+            }
+        }
+        return SecondSmallest;
+
+    }
+
+    public static void main(String[] args) {
+        int[] SampleArray = { 12, 34, 15, 6, 40 };
+        int result = SecondSmallest(SampleArray);
+        System.out.println("Second Largest Element Is: " + result);
+    }
+}
