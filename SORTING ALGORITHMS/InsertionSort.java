75b5d03353834653009b1b61515cea329cb60829
Yash Malvade <yashmalvade91@gmail.com>
2025-05-02 22:11:31 +0530

    commit

---
+++ SORTING ALGORITHMS/InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        // insertion sort
+        for(int i=1;i<arr.length;i++){
+            int current = arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]){
+        arr[j+1] = arr[j];
+        j--;
+            }
+            // placement
+            arr[j+1]=current;
+        }
+        printArray(arr);
+    }
+}
+// Complexity
+// Time Complexity: O(n^2)
+// Auxiliary Space: O(1)
+
